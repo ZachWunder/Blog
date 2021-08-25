f35dae477d76dd6f805dc27825e3c13c15e4807d
Zach Wunder <zachwunder@gmail.com>
2021-08-25 16:19:53 -0700

    Remove top header

--- components/intro.js
+++ components/intro.js
@@ -5,7 +5,7 @@ export default function Intro() {
         Zach's Blog.
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        Programming, business
+         
       </h4>
     </section>
   )
