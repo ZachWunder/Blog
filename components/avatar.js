6d405b4d1f26d71e1566c0559b1c4aad013f7cef
Zach Wunder <zachwunder@gmail.com>
2021-08-29 11:44:52 -0700

    Add dark mode for the logo

--- components/avatar.js
+++ components/avatar.js
@@ -1,8 +1,12 @@
+import AvatarPicture from "./avatarPicture"
+
 export default function Avatar({ name, picture }) {
   return (
     <div className="flex items-center">
-      <img src={picture} className="w-12 h-12 mr-4" alt={name} />
+      <AvatarPicture className="w-12 h-12 mr-4" />
+
       <div className="text-xl font-bold">{name}</div>
     </div>
   )
 }
+
