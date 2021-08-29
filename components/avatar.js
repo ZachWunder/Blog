81b5414d14bec97c1a558b35dadf77c7e2218eda
Zach Wunder <zachwunder@gmail.com>
2021-08-29 13:52:22 -0700

    Optimize image

--- components/avatar.js
+++ components/avatar.js
@@ -3,7 +3,7 @@ import AvatarPicture from "./avatarPicture"
 export default function Avatar({ name, picture }) {
   return (
     <div className="flex items-center">
-      <AvatarPicture className="w-12 h-12 mr-4" />
+      <AvatarPicture />
 
       <div className="text-xl font-bold">{name}</div>
     </div>
