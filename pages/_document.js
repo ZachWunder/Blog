db7316d2391d5644ad35f2717e5471f8d63ab331
Zach Wunder <zachwunder@gmail.com>
2021-11-26 15:47:44 -0800

    add counter.dev analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,13 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { Script } from 'next/script'
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head> 
+          <script>if(!sessionStorage.getItem("_swa")&&document.referrer.indexOf(location.protocol+"//"+location.host)!== 0){fetch("https://counter.dev/track?"+new URLSearchParams({referrer:document.referrer,screen:screen.width+"x"+screen.height,user:"zachwunder",utcoffset:"-8"}))};sessionStorage.setItem("_swa","1");</script>
+        </Head>
         <body>
           <Main />
           <NextScript />
