5b48b84d0656b0cc11497a61bbdf7875849957b5
Zach Wunder <zachwunder@gmail.com>
2021-11-26 16:02:04 -0800

    fix rendering

--- pages/_document.js
+++ pages/_document.js
@@ -6,11 +6,13 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head> 
-          <script>if(!sessionStorage.getItem("_swa")&&document.referrer.indexOf(location.protocol+"//"+location.host)!== 0){fetch("https://counter.dev/track?"+new URLSearchParams({referrer:document.referrer,screen:screen.width+"x"+screen.height,user:"zachwunder",utcoffset:"-8"}))};sessionStorage.setItem("_swa","1");</script>
         </Head>
         <body>
           <Main />
           <NextScript />
+          {
+            typeof window === "undefined" ? null : <script>if(!sessionStorage.getItem("_swa")&&document.referrer.indexOf(location.protocol+"//"+location.host)!== 0){fetch("https://counter.dev/track?"+new URLSearchParams({referrer:document.referrer,screen:screen.width+"x"+screen.height,user:"zachwunder",utcoffset:"-8"}))};sessionStorage.setItem("_swa","1");</script>
+          }
         </body>
       </Html>
     )
