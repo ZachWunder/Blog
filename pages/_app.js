f796310418d8a05ec07c44f0714bedfe12c91ed1
Zach <zach@pod.io>
2022-11-10 10:44:08 -0800

    Add analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
+import { Analytics } from '@vercel/analytics/react';
 import '../styles/index.css'
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  )
 }
